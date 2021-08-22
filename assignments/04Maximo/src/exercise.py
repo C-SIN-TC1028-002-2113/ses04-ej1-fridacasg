0b4ef8bec332a1bfd430b24006b472376ce1f2f5
fridacasg <A01742173@itesm.mx>
2021-08-22 05:49:22 +0000

    EjerciciosSES04

--- assignments/04Maximo/src/exercise.py
+++ assignments/04Maximo/src/exercise.py
@@ -1,6 +1,23 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    num1 = int(input("Ingresa el primer número: "))
+    num2 = int(input("Ingresa el segundo número: "))
+    num3 = int(input("Ingresa el tercer número: "))
+    if num1>=num2:
+        print(num1)
+    elif num2>num3:
+        print(num2)
+    elif num3>num2:
+        print(num3)        
+
+
+            
+
+
+
+      
+  
+    
 
 if __name__=='__main__':
     main()
